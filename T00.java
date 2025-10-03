c71c66269863ad89c0b2119148d36d02dcdba77a
gabrielglenpakpahan-sys <gabrielglenpakpahan@gmail.com>
2025-10-03 14:31:15 +0700

    PAIR 17
    Perbaikan

--- T00.java
+++ T00.java
@@ -1,22 +1,21 @@
 //NIM: 12S25019 NAMA: GEBRIEL GLEN MARCEL PAKPAHAN, NIM: 12S25033 NAMA: MARIA OCTAVIA SIMORANGKIR //
-
 import java.util.*;
 import java.lang.Math;
 
-public class JavaApplication {
+public class T00 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        double bil1, bil2;
+        int bil1, bil2;
 
-        bil1 = Double.parseDouble(input.nextLine());
-        bil2 = Double.parseDouble(input.nextLine());
-        if (Math.abs(bil1) % 2 == 0) {
+        bil1 = Integer.parseInt(input.nextLine());
+        bil2 = Integer.parseInt(input.nextLine());
+        if (bil1 % 2 == 0) {
             System.out.println("Bilangan pertama:Genap");
         } else {
             System.out.println("Bilangan pertama: Ganjil");
         }
-        if (Math.abs(bil2) % 2 == 0) {
+        if (bil2 % 2 == 0) {
             System.out.println("Bilangan kedua: Genap");
         } else {
             System.out.println("Bilangan kedua: Ganjil");
@@ -24,17 +23,17 @@ public class JavaApplication {
         if (bil1 > bil2) {
             System.out.println("Bilangan pertama lebih besar");
         } else {
-            if (bil1 < bil2) {
+            if (bil2 > bil1) {
                 System.out.println("Bilangan kedua lebih besar");
             } else {
-                System.out.println("Berbeda jenis");
+                System.out.println("Kedua bilangan sama besar");
             }
         }
-        if (Math.abs(bil1) % 2 == 0 && Math.abs(bil2) % 2 == 0) {
-            System.out.println("Hasil penjumlahan " + Math.abs(bil1) + Math.abs(bil2));
+        if (bil1 % 2 == 0 && bil2 % 2 == 0) {
+            System.out.println("Hasil penjumlahan " + bil1 + bil2);
         } else {
-            if (Math.abs(bil1) % 2 != 0 && Math.abs(bil2) % 2 != 0) {
-                System.out.println("Hasil perkalian " + Math.abs(bil1) * Math.abs(bil2));
+            if (bil1 % 2 != 0 && bil2 % 2 != 0) {
+                System.out.println("Hasil perkalian " + bil1 * bil2);
             } else {
                 System.out.println("Berbeda jenis");
             }
